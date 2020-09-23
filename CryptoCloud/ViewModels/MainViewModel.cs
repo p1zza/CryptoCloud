8af4c1ce93054e0152ac447a4bd70a2bc918dca7
Vova Ragoulin <vovaragulin@gmail.com>
2020-09-23 19:12:16 +0300

    Добавьте файлы проекта.

---
+++ CryptoCloud/ViewModels/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoCloud.Models
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop ="")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+        private int _Clicks;
+        public int Clicks
+        {
+            get { return _Clicks; }
+            set
+            {
+                _Clicks = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    Task.Delay(1000).Wait();
+                    Clicks++;
+                }
+            });
+        }
+    }
+}
